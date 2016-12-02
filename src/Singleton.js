8307fd16f85b76ffb8afcf1fe5772ecc0f6f4331
scottdomes <scottdomes@gmail.com>
2016-12-02 11:59:14 -0800

    Added Singleton as an alternative to Collection for objects

---
+++ src/Singleton.js
@@ -0,0 +1,81 @@
+import { observable, extendObservable, toJS } from 'mobx'
+import Base from './Base'
+
+// Questions:
+// - storing results array, as list of ID's or as list of objects
+// - where to put collections
+// - subscriptions to updates
+//
+// Basic Usage:
+// - will autoload users based on the provided function.
+// ```
+// const users = new Collection(() => api.loadUsers())
+// users.map(users => user)
+// ```
+export default class Singleton extends Base {
+  _results = observable({})
+  loader
+
+  constructor(loader) {
+    super()
+    
+    this.loader = loader
+    
+    if (typeof this.loader !== 'function') {
+      throw new Error(`Loader is not a function ${this.loader}`)
+    }
+  }
+
+  get results() {
+    this.load()
+    return this._results
+  }
+
+  get keys() {
+    return Object.keys(this._results)
+  }
+
+  get(attribute) {
+    return this.results[attribute]
+  }
+
+  set(attribute, newValue) {
+    this._results[attribute] = newValue
+  }
+
+  assignGetters(model) {
+    Object.keys(model).forEach(key => {
+      if (key !== '_loaded' && key !== '_loading') {
+        Object.defineProperty(this, key, {
+          get: function() { return this._results[key] },
+          set: function(value) { this._results[key] = value }
+        })
+      }
+    })
+  }
+
+  load(force) {
+    return new Promise((resolve, reject) => {
+      
+      // ensure we don't double load
+      if (this.loading) {
+        return resolve(this._results)
+      }
+      
+      if (this.loaded && !force) {
+        return resolve(this._results)
+      }
+      
+      this.setLoading()
+      this.loader()
+        .then(results => {
+          extendObservable(this._results, toJS(results))
+          this.assignGetters(results)
+          
+          this.setLoaded()
+          resolve(this._results)
+        })
+    })
+  }
+
+}
