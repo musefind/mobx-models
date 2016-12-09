56c2024371b11c7f1dbd45f7f9177579e318038f
scottdomes <scottdomes@gmail.com>
2016-12-09 11:08:05 -0800

    Added reactiveComponentWillUpdate

--- src/ReactiveLoaderComponent.js
+++ src/ReactiveLoaderComponent.js
@@ -5,11 +5,22 @@ export default class ReactiveLoaderComponent extends Component{
   componentWillMount() {
     // We give precedence to the supplied componentWillMount- useful for benchmarking
     if (this.reactiveComponentWillMount) {
-        this.reactiveComponentWillMount()
+      this.reactiveComponentWillMount()
     }
 
     LoadDispatch.beginListening()
-    this.render()
+    this.render('test')
+    LoadDispatch.callLoaders()
+    LoadDispatch.endListening()
+  }
+
+  componentWillUpdate() {
+    if (this.reactiveComponentWillUpdate) {
+      this.reactiveComponentWillUpdate()
+    }
+
+    LoadDispatch.beginListening()
+    this.render('test')
     LoadDispatch.callLoaders()
     LoadDispatch.endListening()
   }
