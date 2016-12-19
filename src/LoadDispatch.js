fe775645da03840fee9e2ea04ceffed0dc860c2a
Colin Walker <colinwalker270@gmail.com>
2016-12-19 11:37:57 -0800

    add reactive loader comments and async

--- src/LoadDispatch.js
+++ src/LoadDispatch.js
@@ -3,9 +3,15 @@ class LoadDispatch {
   _listening = false
 
   callLoaders() {
-    this._loaders.forEach(fn => {
-      fn()
-    })
+    this._loaders.forEach(fn => fn())
+  }
+
+  callLoadersAsync() {
+    // this is an 'async' version of call loaders, it sticks the function on
+    // the back of the queue by using setTimeout 0. This is needed to get around
+    // some mobx errors.
+    const copy = this._loaders.slice()
+    setTimeout(() => { copy.forEach(fn => fn()) }, 0)
   }
 
   registerLoader(fn) {
