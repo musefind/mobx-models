e0ea39179e770ab25981e29620527722308d17c9
scottdomes <scottdomes@gmail.com>
2017-01-09 13:59:58 -0800

    reactiveLoader now uses componentDidMount and componentDidUpdate to call loaders

--- src/reactiveLoader.js
+++ src/reactiveLoader.js
@@ -1,40 +1,40 @@
 import {Component} from 'react'
 import LoadDispatch from './LoadDispatch'
+import { observer } from 'mobx-react'
 
-export const asReactiveLoader = (component) => {
+export const asReactiveLoader = (component) => { 
+  const observerComp = observer(component)
+  const base = observerComp.prototype.render
+  const oldDidUpdate = observerComp.prototype.componentDidUpdate
+  const oldDidMount = observerComp.prototype.componentDidMount 
 
+  if (!base) throw new Error("Render must exist on component");
 
-  if (typeof component === 'function' && !component.prototype) {
-    // is stateless functional.
-    return function (props) {
-      LoadDispatch.beginListening()
-      const result = component(props)
+  observerComp.prototype.render = function () {
+    LoadDispatch.beginListening(observerComp.name)
 
-      // need async to keep load requests outside of the mobx-react render.
-      LoadDispatch.callLoadersAsync()
-      LoadDispatch.endListening()
-      return result
-    }
+    // pass the render along to the original render.
+    const result = base.apply(this, arguments)
 
-  } else if (typeof component === 'function') {
-    // is component class
-    const base = component.prototype.render
-
-    if (!base) throw new Error("Render must exist on component");
-
-    component.prototype.render = function () {
-      LoadDispatch.beginListening()
-
-      // pass the render along to the original render.
-      const result = base.apply(this, arguments)
-
-      // need async to keep load requests outside of the mobx-react render.
-      LoadDispatch.callLoadersAsync()
-      LoadDispatch.endListening()
-      return result
-    }
-
-    return component
+    // need async to keep load requests outside of the mobx-react render.
+    LoadDispatch.endListening()
+    return result
   }
+
+  observerComp.prototype.componentDidUpdate = function() {
+    if (oldDidUpdate) {
+      oldDidUpdate.apply(this, arguments)
+    }
+    LoadDispatch.callLoaders(observerComp.name)
+  }
+
+  observerComp.prototype.componentDidMount = function() {
+    if (oldDidMount) {
+      oldDidMount.apply(this, arguments)
+    }
+    LoadDispatch.callLoaders(observerComp.name)
+  }
+
+  return observerComp
 
 }
