bc73a46df473f972c21a80454d90a097a4c041c2
scottdomes <scottdomes@gmail.com>
2017-01-09 15:39:01 -0800

    Fixed name bug with observer component

--- src/reactiveLoader.js
+++ src/reactiveLoader.js
@@ -7,12 +7,16 @@ export const asReactiveLoader = (component) => {
   const base = observerComp.prototype.render
   const oldDidUpdate = observerComp.prototype.componentDidUpdate
   const oldDidMount = observerComp.prototype.componentDidMount 
+  var name = component.name || component.displayName
 
   if (!base) throw new Error("Render must exist on component");
-  if (!observerComp.name) throw new Error("Component name is required to load");
+  if (!name) throw new Error("Component name is required to load");
 
   observerComp.prototype.render = function () {
-    LoadDispatch.beginListening(observerComp.name)
+    if (!this._loaderID) {
+      this._loaderID = name + Math.random().toFixed(5)
+    }
+    LoadDispatch.beginListening(this._loaderID)
 
     // pass the render along to the original render.
     const result = base.apply(this, arguments)
@@ -26,14 +30,14 @@ export const asReactiveLoader = (component) => {
     if (oldDidUpdate) {
       oldDidUpdate.apply(this, arguments)
     }
-    LoadDispatch.callLoaders(observerComp.name)
+    LoadDispatch.callLoaders(this._loaderID)
   }
 
   observerComp.prototype.componentDidMount = function() {
     if (oldDidMount) {
       oldDidMount.apply(this, arguments)
     }
-    LoadDispatch.callLoaders(observerComp.name)
+    LoadDispatch.callLoaders(this._loaderID)
   }
 
   return observerComp
