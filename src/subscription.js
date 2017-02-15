e115abbbaf046511455fac9780dc508fca64fd64
Colin Walker <colinwalker270@gmail.com>
2017-02-15 11:05:33 -0800

    save tests

--- src/subscription.js
+++ src/subscription.js
@@ -1,2 +1,30 @@
-export const subscribe = (object, callback) => {
+import { spy } from 'mobx'
+import Model from './Model'
+
+const subscriptions = []
+
+const loggingSpy = (evt) => {
+  switch (evt.type) {
+    case 'add':
+    case 'update':
+    case 'splice':
+    case 'delete':
+      if (evt.object instanceof Model) {
+        console.log(evt.type, `${evt.object.constructor.name}(${evt.object.id})`, evt.name)
+      }
+      break
+    case 'action':
+      let name = 'Object'
+      if (evt.target instanceof Model) {
+        name = `${evt.target.constructor.name}(${evt.target.id})`
+      }
+      console.log('action', name, evt.name)
+      break
+    default:
+      console.log(evt)
+  }
+}
+
+export const logger = () => {
+  return spy(loggingSpy)
 }
--- src/subscription.test.js
+++ src/subscription.test.js
@@ -1,13 +1,34 @@
 import assert from 'assert'
-import { isObservable, extendObservable } from 'mobx'
+import { isObservable, extendObservable, observable, spy, action, Atom } from 'mobx'
+import Model from './Model'
+import { logger } from './subscription'
+
+class User extends Model {
+  retrieve() {
+    this.name = 'newname'
+    return Promise.resolve({})
+  }
+
+  @action setName(name) {
+    this.name = name
+  }
+}
 
 describe('subscription', () => {
 
-  it('explains observable array properties', () => {
-    const object = extendObservable({
-      list: [1, 2, 3],
-    })
-    console.log(object)
+  it('explains observable properties', () => {
+    const atom = new Atom("TestAtom")
+
+    const dispose = logger()
+
+    const user = User.initialize({id: 1, name: 'Colin'})
+    // const user2 = User.initialize({id: 2, name: 'Colin'})
+    // user.load()
+    user.setName('newname')
+
+    atom.reportChanged()
+
+    dispose()
   })
 
 })
