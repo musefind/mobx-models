6637a09a67e4b96586a4d0f7aae0d87547b24930
Colin Walker <colinwalker270@gmail.com>
2017-02-16 10:52:34 -0800

    add docs for load dispatch method

--- src/LoadDispatch.js
+++ src/LoadDispatch.js
@@ -10,11 +10,13 @@
  *   
  *   render() {
  *     LoadDispatch.beginListening(this.id)
- *     return (
+ *     const results = (
  *       <div>
  *         {this.props.collection.map(...)}
  *       </div>
  *     )
+ *     LoadDispatch.endListening()
+ *     return results
  *   }
  * }
  */
@@ -23,24 +25,39 @@ class LoadDispatch {
   _listening = false
   _currentComponent = undefined
 
+  /**
+   * Call the loaders for a specific component.
+   * @param {String} component
+   */
   callLoaders(component) {
     this._loaders[component].forEach(fn => {
       fn()
     })
   }
 
+  /**
+   * Register a loader, this is a function that should load data for a specific object.
+   * @param {Function} fn
+   */
   registerLoader(fn) {
     if (this._listening) {
       this._loaders[this._currentComponent].push(fn)
     }
   }
 
+  /**
+   * Begin listening to a component.
+   * @param {String} component
+   */
   beginListening(component) {
     this._currentComponent = component
     this._listening = true
     this._loaders[component] = []
   }
 
+  /**
+   * End listening to a component.
+   */
   endListening() {
     this._listening = false
     this._currentComponent = undefined
